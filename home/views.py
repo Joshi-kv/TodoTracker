2486bd0498a3f0e26be7e9a67f5f037fff206716
JOshi kv <joshikv3705@gmail.com>
2023-06-27 10:09:25 +0530

    main page design

--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import View
 
 # Create your views here.
-def index(request) : 
-    return render(request,'index.html')
+
+class HomePageView(View) : 
+    def get(self,request) : 
+        return render(request,'index.html')
