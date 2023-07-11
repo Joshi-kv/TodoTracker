c4fc2d4d1d96957669a9e16a60ac651d9ca68326
JOshi kv <joshikv3705@gmail.com>
2023-07-11 10:16:07 +0530

    data table pagination bug

--- static/assets/js/todo-list.js
+++ static/assets/js/todo-list.js
@@ -16,8 +16,35 @@ $(document).ready(() =>{
                 <button class="btn btn-danger" id="editBtn" data-bs-target="#updateModal" data-bs-toggle="modal" data-edit="${task.task_id}"><i class="fas fa-edit"></i></button>
                 <button class="btn btn-primary my-1" id="deleteBtn" data-bs-target="#deleteModal" data-bs-toggle="modal" data-delete="${task.task_id}"><i class="fas fa-trash"></i></button>
                 `
-              ]).draw()
-            //   $('taskTable').node().id=`${task.task_id}`;
+            ]).draw()
+            if(data.tasks.length <= $('select[name="taskTable_length"]').val() ){
+                $('.pagination').hide()
+            }else{
+                $('.pagination').show()
+            }
+            
+            // $('#taskTable').on('change',(e) =>{
+            //     console.log('change')
+            // })
+
+            // if(data.tasks.length <= 10){
+            //     $('.dataTables_length').hide()
+            // }else{
+            //     $('.dataTables_length').show()
+            // }
+            // if(data.tasks.length <= $('select[name="taskTable_length"]').val() ){
+            //     $('.pagination').hide()
+            // }else{
+            //     $('.pagination').show()
+            // }
+            $('select[name="taskTable_length"]').on('change',() =>{
+                console.log($('select[name="taskTable_length').val())
+                if(data.tasks.length <= $('select[name="taskTable_length"]').val() ){
+                    $('.pagination').hide()
+                }else{
+                    $('.pagination').show()
+                }
+            })
         })
     })
 })
