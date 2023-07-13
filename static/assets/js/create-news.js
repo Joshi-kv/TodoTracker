ff8e0ae3c991d2062c2a5dd865a578120b6b5992
JOshi kv <joshikv3705@gmail.com>
2023-07-13 09:35:35 +0530

    news image validate error fixed

--- static/assets/js/create-news.js
+++ static/assets/js/create-news.js
@@ -21,9 +21,19 @@ $(document).ready(() => {
             $(element).addClass("is-invalid").removeClass("is-valid");
         },
         unhighlight: function(element) {
+            if($(element).attr('file')){
+                $('#file-error').hide()
+            }
             $(element).removeClass('is-invalid')
         },
         errorPlacement : function(error,element){
+            if(element.attr('name') == 'newsImage'){
+                if(element.val() == ''){
+                    error.addClass('text-danger')
+                }else{
+                    error.remove()
+                }
+            }
             error.addClass(' text-danger')
             error.insertBefore(element)
         },
