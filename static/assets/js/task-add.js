d9f71333208ce3da6240fa2b438e6f18256234dc
JOshi kv <joshikv3705@gmail.com>
2023-07-14 09:36:54 +0530

    adding attribute to table row

--- static/assets/js/task-add.js
+++ static/assets/js/task-add.js
@@ -68,7 +68,8 @@ $(document).ready(() => {
                 let total = response.total
                 let table = $('#taskTable').DataTable();
                 let convertedTaskDuedate = moment(task.task_duedate).format('DD/MM/yy');
-                table.row.add([
+                let taskId = task.task_id
+                let newRow = table.row.add([
                     `${task.task_title}`,
                     `${task.task_description}`,
                     `${convertedTaskDuedate}`,
@@ -78,8 +79,9 @@ $(document).ready(() => {
                     <button class="btn btn-danger" id="editBtn" data-bs-target="#updateModal" data-bs-toggle="modal" data-edit="${task.task_id}"><i class="fas fa-edit"></i></button>
                     <button class="btn btn-primary my-1" id="deleteBtn" data-bs-target="#deleteModal" data-bs-toggle="modal" data-delete="${task.task_id}"><i class="fas fa-trash"></i></button>
                     `
-                ]).draw();
-
+                ]).node();
+                $(newRow).attr('data-task-id',taskId)
+                table.draw()
                 alertify.set('notifier', 'position', 'top-right');
                 alertify.success('New task added successfully');
 
