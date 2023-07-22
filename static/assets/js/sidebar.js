41e483005ef653c9b4b25527e337857ea35f60d3
JOshi kv <joshikv3705@gmail.com>
2023-07-22 15:34:37 +0530

    active sidebar menu highlight bug fixed

--- static/assets/js/sidebar.js
+++ static/assets/js/sidebar.js
@@ -3,7 +3,12 @@ $(document).ready(() => {
     var navLinks = document.querySelectorAll('.sidebar-nav .nav-link')
     var windowPathname = window.location.pathname
 
-    navLinks.forEach((navLink) => {        
+    navLinks.forEach((navLink) => {      
+        
+        if(windowPathname === '/announcements/' || windowPathname == '/my-news/'){
+            document.getElementById('newsMenu').classList.add('active-menu')
+        }
+        
         if(navLink.getAttribute('href') == windowPathname ){
             navLink.classList.add('active-menu')
         }
